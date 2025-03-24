031c5bfada7db6e9e79ad2f021efabe48da1cd4e
RevanthKanaparthy <revanthkanaparthy44@gmail.com>
2025-03-24 13:03:44 +0530

    Update cmd2.java

--- cmd2.java
+++ cmd2.java
@@ -1,18 +1,17 @@
-class CommandLineDemo2
-{
+class CommandLineDemo2 {
+    public static void main(String args[]) {
+        // Check if the correct number of arguments is provided
+        if (args.length < 3) {
+            System.out.println("Please provide at least three arguments: Name, Rno, and Dept.");
+            return;
+        }
 
-public static void main(String args[])
+        String s1 = args[0]; 
+        String s2 = args[1]; 
+        String s3 = args[2]; 
 
-String s1=args[0];
-
-String s2=args[1];
-
-String s3=args[2];
-
-System.out.println("Name:"+s1);
-
-System.out.println("Rno:"+s2);
-
-System.out.println("Dept:"+s3);
-}
+        System.out.println("Name: " + s1);
+        System.out.println("Rno: " + s2);
+        System.out.println("Dept: " + s3);
+    }
 }
